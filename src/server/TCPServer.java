faaac10ca3b3619c86244a6fe549d311895b53f4
maximirm <maxim91@hotmail.de>
2020-12-13 16:35:09 +0100

    small changes

--- src/server/TCPServer.java
+++ src/server/TCPServer.java
@@ -24,17 +24,28 @@ public class TCPServer {
     private void doSomething() throws IOException, InterruptedException {
         //create socket
         ServerSocket srvSocket = new ServerSocket(this.port);
+        System.out.println("server socket created");
+
         //open port -> Socket represents connection with the connected client
         Socket socket = srvSocket.accept();
+        System.out.println("connection to client accepted");
+
         //read from socket e.g. GET/index.. / ignore result in this example
         socket.getInputStream().read();
+        System.out.println("read from inputStream");
 
         //get outputStream from socket
         OutputStream os = socket.getOutputStream();
-        //respond -> write into os
         os.write("hey mister client".getBytes());
+
+        //respond -> write into os
+        System.out.println("wrote in outputStream");
+        System.out.println("going to sleep");
+
         //put process to sleep for a while
         Thread.sleep(5000);
+        System.out.println("woke up");
+        
         //close outputStream
         os.close();
     }
